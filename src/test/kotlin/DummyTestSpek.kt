7437667baa1badb6205ba88a7c4664aa6860b1ef
ahocquard <alexandre.hocquard@akeneo.com>
2018-11-12 22:36:57 +0100

    Initialize spek

---
+++ src/test/kotlin/DummyTestSpek.kt
@@ -0,0 +1,15 @@
+//import junit.framework.Assert.assertEquals
+//import org.spekframework.spek2.Spek
+//import org.spekframework.spek2.style.gherkin.Feature
+//
+//class DummyTestSpek : Spek({
+//    Feature("Set") {
+//        val set by memoized { mutableSetOf<String>() }
+//
+//        Scenario("empty") {
+//            Then("should have a size of 0") {
+//                assertEquals(0, set.size)
+//            }
+//        }
+//    }
+//})
