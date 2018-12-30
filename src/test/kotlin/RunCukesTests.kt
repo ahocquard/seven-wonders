822db0f897daf79ead499885dba9fb2028236a02
ahocquard <alexandre.hocquard@akeneo.com>
2018-12-30 15:26:06 +0100

    Create the different use cases to setup a new party

--- src/test/kotlin/RunCukesTests.kt
+++ src/test/kotlin/RunCukesTests.kt
@@ -5,5 +5,8 @@ import cucumber.api.junit.Cucumber
 import org.junit.runner.RunWith
 
 @RunWith(Cucumber::class)
-@CucumberOptions(features = ["use_cases"])
+@CucumberOptions(
+    features = ["use_cases"],
+    tags = ["@in_progress and not @todo"]
+)
 class RunCukesTests
---
+++ src/test/kotlin/context/setup/SetupCoinDefinition.kt
@@ -0,0 +1,9 @@
+package cucumber
+
+import cucumber.api.java8.En
+
+class SetupCoinDefinition : En {
+    init {
+
+    }
+}
